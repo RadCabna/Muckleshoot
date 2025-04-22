169301cca20d411081b6a286de8f59f5f50ee69e
Radcabna <radcabna@gmail.com>
2025-04-22 13:13:53 +0300

    Initial Commit

---
+++ Muckleshoot-game/Muckleshoot_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muckleshoot_gameApp.swift
+//  Muckleshoot-game
+//
+//  Created by Алкександр Степанов on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Muckleshoot_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
